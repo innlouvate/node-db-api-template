70f205eab8415ec228948e5e85e1dd3f030a88d6
Lou Franklin <franklin@thoughtworks.com>
2017-08-21 15:58:50 +0100

    get all entires

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var router = express.Router();
-
+var queries = require('../db/queries');
 
 // *** GET all shows *** //
 router.get('/shows', function(req, res, next) {
-  res.send('send shows back');
+  queries.getAll()
+  .then(function(shows) {
+    res.status(200).json(shows);
+  })
+  .catch(function(error) {
+    next(error);
+  });
 });
-
 
 module.exports = router;
